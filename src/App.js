626dabcf3063d1f7c7ce1d2c8117bcce435b5c81
James Morcom <morcs@morcs.com>
2018-02-25 18:10:11 +0000

    Ability to add entries.

--- src/App.js
+++ src/App.js
@@ -3,13 +3,35 @@ import './App.css';
 
 class App extends Component {
 
-  entries = [
-    {
-      date: '25 Feb 2018',
-      intensity: 8,
-      duration: 20,
+  constructor(props) {
+    super(props);
+    
+    this.state = { 
+      date: new Date(),
+      intensity: null,
+      duration: null,
+      entries: []
     }
-  ]
+
+    this.addEntry = this.addEntry.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    })
+  }
+
+  addEntry() {
+    this.setState(prevState => ({
+      entries: [...prevState.entries, { date: this.state.date, intensity: this.state.intensity, duration: this.state.duration }]
+    }));
+  }
 
   render() {
     return (
@@ -24,7 +46,7 @@ class App extends Component {
             </tr>
           </thead>
           <tbody>
-            {this.entries.map(e => 
+            {this.state.entries.map(e => 
               (<tr>
                 <td>{e.date}</td>
                 <td>{e.intensity}</td>
@@ -32,6 +54,12 @@ class App extends Component {
                 <td>{e.intensity * e.duration}</td>
               </tr>)
             )}
+            <tr>
+              <td><input name="date" type="date" className="form-control" value={this.state.date} onChange={this.handleInputChange} /></td>
+              <td><input name="intensity" type="number" className="form-control" value={this.state.intensity} onChange={this.handleInputChange} /></td>
+              <td><input name="duration" type="number" className="form-control" value={this.state.duration} onChange={this.handleInputChange} /></td>
+              <td><input type="submit" value="Add" className="btn btn-primary" onClick={this.addEntry} /></td>
+            </tr>
           </tbody>
         </table>
       </div>
