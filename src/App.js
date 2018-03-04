5b6c4f8f9691539593ed46624c8c2e4fc3aad19e
James Morcom <morcs@morcs.com>
2018-03-04 11:01:22 +0000

    Show a week total.

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ type AppState =
 type Props = { }
 
 type Session = {
-  intensity: ?number,
+  intensity: number,
   duration: ?number
 }
 
@@ -39,6 +39,7 @@ class App extends Component<Props, State> {
     this.removeSession = this.removeSession.bind(this);
     this.undefinedToEmpty = this.undefinedToEmpty.bind(this);
     this.generate4Weeks = this.generate4Weeks.bind(this);
+    this.renderSessionForm = this.renderSessionForm.bind(this);
   }
 
   addSession = () => {
@@ -82,67 +83,84 @@ class App extends Component<Props, State> {
     }));
   }
 
+  getSessionTotal(session: Session) {
+    if(session.duration != null)
+    {
+      return session.intensity * session.duration;
+    }
+  }
+
+  getWeekTotal(week: Week) {
+    return week.sessions.map(this.getSessionTotal).reduce((acc, val) => acc + val);
+  }
+
   undefinedToEmpty = (value: any) => {
     return !value && value !== 0 ? '' : value;
   }
 
+  renderSessionForm = (session: Session, index: number) => (
+    <div className="panel panel-default" key={index}>
+      <div className="panel-heading">
+        Session {index + 1}
+        <span className="glyphicon glyphicon-remove pull-right" aria-hidden="true" onClick={() => this.removeSession(index)}></span>
+      </div>
+      <div className="panel-body">
+        <div className="form-group">
+          <label htmlFor="intensity">Intensity/sRPE (1-10)</label>
+          <select name="intensity" value={session.intensity} className="form-control" id="intensity" onChange={(event) => this.updateSession(event, index)}>
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+            <option>6</option>
+            <option>7</option>
+            <option>8</option>
+            <option>9</option>
+            <option>10</option>
+          </select>
+        </div>
+        {' '}
+        <div className="form-group">
+          <label htmlFor="duration">Duration (mins)</label>
+          <input name="duration" type="number" value={this.undefinedToEmpty(session.duration)} className="form-control" id="duration" placeholder="Duration (mins)" onChange={(event) => this.updateSession(event, index)} required />
+        </div>
+      </div>
+    </div>
+  );
+
   render() {
-    return this.state.app === 'Initial' 
-      ? (
-        <div className="container">
-          <p>Start by telling us what a typical training week involves</p>
-          <form onSubmit={e => this.generate4Weeks(e, this.state.sessions)}>
-          {this.state.sessions.map((session, index) => {
-            return (
-              <div className="panel panel-default" key={index}>
-                <div className="panel-heading">
-                  Session {index + 1}
-                  <span className="glyphicon glyphicon-remove pull-right" aria-hidden="true" onClick={() => this.removeSession(index)}></span>
-                </div>
-                <div className="panel-body">
-                  <div className="form-group">
-                    <label htmlFor="intensity">Intensity/sRPE (1-10)</label>
-                    <select name="intensity" value={session.intensity} className="form-control" id="intensity" onChange={(event) => this.updateSession(event, index)}>
-                      <option>1</option>
-                      <option>2</option>
-                      <option>3</option>
-                      <option>4</option>
-                      <option>5</option>
-                      <option>6</option>
-                      <option>7</option>
-                      <option>8</option>
-                      <option>9</option>
-                      <option>10</option>
-                    </select>
-                  </div>
-                  {' '}
-                  <div className="form-group">
-                    <label htmlFor="duration">Duration (mins)</label>
-                    <input name="duration" type="number" value={this.undefinedToEmpty(session.duration)} className="form-control" id="duration" placeholder="Duration (mins)" onChange={(event) => this.updateSession(event, index)} required />
-                  </div>
-                </div>
-              </div>
-            );
-          })}
-          <div className="row form-group">
-            <div className="col-sm-12">
-              <button type="button" className="btn btn-default" onClick={this.addSession}>
-                <span className="glyphicon glyphicon-plus" aria-hidden="true"></span> Add session
-              </button>
-              {this.state.sessions.length > 0 && 
-                <button className="btn btn-primary pull-right">
-                  <span className="glyphicon glyphicon-ok" aria-hidden="true"></span> Week complete
+    return (
+      <div className="container">
+      { this.state.app === 'Initial' 
+        ? (
+          <div>
+            <p>Start by telling us what a typical training week involves</p>
+            <form onSubmit={e => this.generate4Weeks(e, this.state.sessions)}>
+            {this.state.sessions.map((session, index) => this.renderSessionForm(session,index))}
+            <div className="row form-group">
+              <div className="col-sm-12">
+                <button type="button" className="btn btn-default" onClick={this.addSession}>
+                  <span className="glyphicon glyphicon-plus" aria-hidden="true"></span> Add session
                 </button>
-              }
+                {this.state.sessions.length > 0 && 
+                  <button className="btn btn-primary pull-right">
+                    <span className="glyphicon glyphicon-ok" aria-hidden="true"></span> Week complete
+                  </button>
+                }
+              </div>
             </div>
+            </form>
           </div>
-          </form>
-        </div>)
+        )
         : (
           <div>{this.state.weeks.map((week, i) => (
-            <div key={i}>{week.sessions.length}</div>
+            <div key={i}>
+              <h3>{this.state.weeks.length - i} weeks ago ({this.getWeekTotal(week)})</h3>
+            </div>
           ))}</div>
-        );
+        )}
+        </div>);
   }
 }
 
