0e11fd2569a7950065837cace71cce1a9484943f
James Morcom <morcs@morcs.com>
2018-03-16 19:09:13 +0000

    Make form horizontal.

--- src/components/SessionForm.js
+++ src/components/SessionForm.js
@@ -20,26 +20,30 @@ const SessionForm = ({ session, index, removeSession, updateSession }: Props) =>
           Session {index + 1}
           <span className="glyphicon glyphicon-remove pull-right" aria-hidden="true" onClick={() => removeSession(index)}></span>
         </div>
-        <div className="panel-body">
+        <div className="panel-body form-horizontal">
           <div className="form-group">
-            <label htmlFor="intensity">Intensity/sRPE (1-10)</label>
-            <select name="intensity" value={session.intensity} className="form-control" id="intensity" onChange={(event) => updateSession(event, index)}>
-              <option>1</option>
-              <option>2</option>
-              <option>3</option>
-              <option>4</option>
-              <option>5</option>
-              <option>6</option>
-              <option>7</option>
-              <option>8</option>
-              <option>9</option>
-              <option>10</option>
-            </select>
+            <label htmlFor="intensity" className="col-sm-3 control-label">Intensity/sRPE (1-10)</label>
+            <div className="col-sm-9">
+              <select name="intensity" value={session.intensity} className="form-control" id="intensity" onChange={(event) => updateSession(event, index)}>
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+                <option>6</option>
+                <option>7</option>
+                <option>8</option>
+                <option>9</option>
+                <option>10</option>
+              </select>
+            </div>
           </div>
           {' '}
           <div className="form-group">
-            <label htmlFor="duration">Duration (mins)</label>
-            <input name="duration" type="number" value={undefinedToEmpty(session.duration)} className="form-control" id="duration" placeholder="Duration (mins)" onChange={(event) => updateSession(event, index)} required />
+            <label htmlFor="duration" className="col-sm-3 control-label">Duration (mins)</label>
+            <div className="col-sm-9">
+              <input name="duration" type="number" value={undefinedToEmpty(session.duration)} className="form-control" id="duration" placeholder="Duration (mins)" onChange={(event) => updateSession(event, index)} required />
+            </div>
           </div>
         </div>
       </div>
